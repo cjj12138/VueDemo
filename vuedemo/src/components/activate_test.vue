58922970257a0fe1157c98d97c67d5336d866301
chen <1215716506@qq.com>
2019-11-25 12:40:11 +0800

    管理员激活与关闭考试前后端交互

--- vuedemo/src/components/activate_test.vue
+++ vuedemo/src/components/activate_test.vue
@@ -1,43 +1,24 @@
-<!-- <template>
-	<el-table :data="tableData" border style="width: 100%">
-		<el-table-column prop="course_name" label="考试名称" width="180"></el-table-column>
-		<el-table-column prop="test_date" label="考试日期" width="180"></el-table-column>
-		<el-table-column prop="test_starttime" label="考试开始时间"></el-table-column>
-		<el-table-column prop="test_endtime" label="考试结束时间"></el-table-column>
-		<el-table-column prop="test_place" label="考试地点"></el-table-column>
-		<el-table-column property="state" align="center" label="权限状态">
-			<template slot-scope="scope">
-				<el-switch
-					v-model="tableData[scope.$index].state"
-					:active-value="1"
-					:inactive-value="0"
-					active-color="#13ce66"
-					inactive-color="#ff4949"
-				></el-switch>
-			</template>
-		</el-table-column>
-	</el-table>
-</template> -->
+
 <template>
     <el-table :data="tableData" border style="width: 100%">
-      <el-table-column label="考试名称" width="160">
+      <el-table-column label="考试名称" align="center">
         <template scope="scope">
           <span style="margin-left: 10px">{{ scope.row.course_name }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="考试日期" width="180">
+      <el-table-column label="考试日期" align="center">
         <template scope="scope">
           <span style="margin-left: 10px">{{ scope.row.test_date }}</span>
           </el-popover>
         </template>
       </el-table-column>
-	  <el-table-column label="考试开始时间" width="180">
+	  <el-table-column label="考试开始时间" align="center">
 	    <template scope="scope">
 	      <span style="margin-left: 10px">{{ scope.row.test_starttime }}</span>
 	      </el-popover>
 	    </template>
 	  </el-table-column>
-	  <el-table-column label="考试结束日期" width="180">
+	  <el-table-column label="考试结束日期" align="center">
 	    <template scope="scope">
 	      <span style="margin-left: 10px">{{ scope.row.test_endtime }}</span>
 	      </el-popover>
@@ -46,9 +27,13 @@
       <el-table-column label="状态" align="center">
         <template scope="props">
           <el-switch
+			class="switchStyle"
             v-model="tableData[props.$index].state"
+			active-text="开启" 
+			inactive-text="关闭"
             :active-value="1"
-            :inactive-value="0">
+            :inactive-value="0"
+			@change="changeSwitch(props.$index,props.row)">
           </el-switch>
         </template>
       </el-table-column>
@@ -61,22 +46,42 @@ import axios from 'axios';
 export default {
 	data() {
 		return {
-			tableData: [
-				
-			]
+			tableData: []
 		};
 	},
 	mounted: function() {
 		this.showtable();
 	},
 	methods: {
-		change(index, row) {
-			console.log(row);
+		changeSwitch(index, row) {
+			console.log(row.state);
+			var that = this;
+			if(row.state==0){
+				console.log("关闭")
+				axios({
+					method: 'post',
+					url: '/api/end_bm',
+					data:{
+						test_id:row.test_id,
+						state:0,
+					}
+				})
+			}else{
+				console.log("开始")
+				axios({
+					method: 'post',
+					url: '/api/start_bm',
+					data:{
+						test_id:row.test_id,
+						state:1,
+					}
+				})
+			}
 		},
 		showtable() {
 			var that = this;
 			axios({
-				method: 'get',
+				method: 'post',
 				url: '/api/registration_Adm'
 			}).then(function(response) {
 				console.log(response);
@@ -87,3 +92,26 @@ export default {
 	}
 };
 </script>
+<style>
+	.switchStyle .el-switch__label {
+	  position: absolute;
+	  display: none;
+	  color: #fff;
+	}
+	.switchStyle .el-switch__label--left {
+	  z-index: 12;
+	  left: 6px;
+	}
+	.switchStyle .el-switch__label--right {
+	  z-index: 12;
+	  left: -14px;
+	}
+	.switchStyle .el-switch__label.is-active {
+	  display: block;
+	}
+	.switchStyle.el-switch .el-switch__core,
+	.el-switch .el-switch__label {
+	  width: 60px !important;
+	}
+</style>
+
